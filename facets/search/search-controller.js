b4a2071c24946e69f0594a5cb1b3272fb019b217
faiq <faiqrazarizvi@gmail.com>
2014-06-09 15:26:27 -0700

    fixing elasticsearch

--- facets/search/search-controller.js
+++ facets/search/search-controller.js
@@ -1,74 +1,82 @@
 var elasticsearch = require('elasticsearch'), 
-    config = require('../../config'),
-    client = new elasticsearch.Client({
-      host: config.search.url
-    });
+config = require('../../config'),
+client = new elasticsearch.Client({
+  host: config.search.url
+});
 
 module.exports = function(request, reply){
-  var page = parseInt(request.page || '0', 10); 
-  client.search({
-  fields : ['name', 'keywords','description','author','version', 'stars', 'dlScore', 'dlDay', 'dlWeek'],
-  query :{ 
-        "dis_max": {
+
+  var searchQuery = {
+    fields : ['name', 'keywords','description','author','version', 'stars', 'dlScore', 'dlDay', 'dlWeek'],
+    body: { 
+      "query" :{ 
+      "dis_max": {
         "tie_breaker": 0.7,
         "boost": 1.2,
         "queries": [
-          {
-            "function_score": {
-              "query": {
-                "match": {
-                  "name.untouched": request.q
-                }
-              },
-              "boost_factor": 100
-            }
-          },
-          {
-            "bool": {
-              "should": [
-                {"match_phrase": {"name": request.q} },
-                {"match_phrase": {"keywords": request.q} },
-                {"match_phrase": {"description": request.q} },
-                {"match_phrase": {"readme": request.q} }
-              ],
-              "minimum_should_match": 1,
-              "boost": 50
-            }
-          },
         {
-            "function_score": {
-              "query": {
-                "multi_match": {
-                  "query": request.q,
-                  "fields": ["name^4", "keywords", "description", "readme"]
-                }
-              },
-              "functions": [
-                {
-                  "script_score": {
-                    "script": "(doc['dlScore'].isEmpty() ? 0 : doc['dlScore'].value)"
-                  }
-                },
-                {
-                  "script_score": {
-                    "script": "doc['stars'].isEmpty() ? 0 : doc['stars'].value"
-                  }
-                }
-              ],
-              "score_mode": "sum",
-              "boost_mode": "multiply"
-            }
+          "function_score": {
+            "query": {
+              "match": {
+                "name.untouched": request.query.q
+              }
+            },
+            "boost_factor": 100
           }
+        },
+        {
+          "bool": {
+            "should": [
+            {"match_phrase": {"name": request.query.q} },
+            {"match_phrase": {"keywords": request.query.q} },
+            {"match_phrase": {"description": request.query.q} },
+            {"match_phrase": {"readme": request.query.q} }
+            ],
+            "minimum_should_match": 1,
+            "boost": 50
+          }
+        },
+        {
+          "function_score": {
+            "query": {
+              "multi_match": {
+                "query": request.query.q,
+                "fields": ["name^4", "keywords", "description", "readme"]
+              }
+            },
+            "functions": [
+            {
+              "script_score": {
+                "script": "(doc['dlScore'].isEmpty() ? 0 : doc['dlScore'].value)"
+              }
+            },
+            {
+              "script_score": {
+                "script": "doc['stars'].isEmpty() ? 0 : doc['stars'].value"
+              }
+            }
+            ],
+            "score_mode": "sum",
+            "boost_mode": "multiply"
+          }
+        }
         ]
       }
-  }}, function (error, response){
+    }
+  }
+};
+
+  client.search(searchQuery, function (error, response){
    if (error) { 
       reply("error with elastic search"); //change this later, when I get the thign workign 
-    return;
+      console.log(error); 
+      return;
     }
+    console.log("showing results"); 
+    console.log(response);
     console.log(response.hits);
     reply.view("index", {
-        hits: response.hits
-      });  
-});
+      hits: response.hits
+    });  
+  }); 
 }
